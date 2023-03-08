89dffa66d350a15f30ceaafd6cc928ad805310a0
TheRealGeneParmesan <Geneparmigiano@gmail.com>
2023-03-08 11:01:14 -0800

    added router method to my html routes and exported router

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,12 +1,17 @@
 const path = require('path');
+const router = require('express').Router();
 // const notesRouter = require('../server')
 
+
+
+// /notes sends us to the notes.html file. 
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
 // * matches any URL that hasn't been defined and sends us to the home page 
-app.get('*', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-// /notes sends us to the notes.html file. 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const apiRoute = require('./routes/apiRoute');
-const indexRoute = require('./routes/indexRoute');
+const apiRoute = require('./routes/apiRoute.js');
+const indexRoute = require('./routes/indexRoute.js');
 
 const PORT = process.env.port || 3001;
 
@@ -9,10 +9,10 @@ const app = express();
 // Middleware for parsing JSON and urlencoded request data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use('/api', apiRoute);
 app.use('/', indexRoute);
-app.use(express.static('public'));
 
 app.listen(PORT, () =>
     console.log(`App listening on port ${PORT}`)
-);
+); 
